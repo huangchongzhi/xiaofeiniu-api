4610a3ed236e4f5a46f8e5993b60db32959e8ee5
huangchognzhi <545463097@qq.com>
2019-01-28 16:15:27 +0800

    2019-1-28 16:15:25

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -8,6 +8,7 @@ const router=express.Router();
 /**
  * API：GET /admin/login
  * 请求数据：{aname:'xxx',apwd:'xxx'}
+ * 完成用户登录验证
  * 返回数据：
  * {code:200,msg:'login succ'}
  * {code:400,msg:'aname or apwd err'}
@@ -15,6 +16,15 @@ const router=express.Router();
 router.get("/logon",(req,res)=>{
     pool.query(url,data,callback);
 })
+
+/**
+ * API：PATCH /admin/login  （put 修改全部、patch只修改部分）
+ * 请求数据：{aname:'xxx',apwd:'xxx'}
+ * 根据管理员名和密码修改管理员密码
+ * 返回数据：
+ * {code:200,msg:'login succ'}
+ * {code:400,msg:'aname or apwd err'}
+ */
 
 // 导出
 module.exports = router;
