c452970bc59670c084bc425e7bfddf7707e1e607
huangchognzhi <545463097@qq.com>
2019-01-28 16:12:23 +0800

    2019-1-28 16:12:21

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,7 +1,20 @@
+/*
+* 管理员相关路由
+*/
 const express=require('express');
-const router=express.Router();
 const pool=require("../../pool.js");
+const router=express.Router();
 
-router.get("/admin",(req,res)=>{
-
+/**
+ * API：GET /admin/login
+ * 请求数据：{aname:'xxx',apwd:'xxx'}
+ * 返回数据：
+ * {code:200,msg:'login succ'}
+ * {code:400,msg:'aname or apwd err'}
+ */
+router.get("/logon",(req,res)=>{
+    pool.query(url,data,callback);
 })
+
+// 导出
+module.exports = router;
